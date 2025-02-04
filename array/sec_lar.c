f20e147867f9be847713e4d5c74454c2f2d58bc4
Amrita <amritans2017@gmail.com>
2025-02-04 13:02:08 +0530

    add

--- array/sec_lar.c
+++ array/sec_lar.c
@@ -1,3 +1,26 @@
+/* 
+  Write a program to find second largest element in the array
+  
+  Name : Amrita N S
+  Date : 04-02-2025
+  
+  Description : 
+   
+     *Program prompts user for an integer array of 5 elements
+   
+     *find largest element in the array
+   
+     *finally find second largest element in the array
+  
+  Sample input : 
+  Enter the array elements 20 10 5 3 2 
+  
+  Sample output :
+  Largest element is 20
+  Second largest element is 10
+ 
+*/
+
 #include <stdio.h>
 
 int main() 
